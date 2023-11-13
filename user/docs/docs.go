53dbdc671cf63ce3877b92e1c8915c35b0992f07
leon123858 <a0970785699@gmail.com>
2023-11-13 22:57:43 +0800

    update

--- user/docs/docs.go
+++ user/docs/docs.go
@@ -368,7 +368,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "3.0",
+	Version:          "2.0",
 	Host:             "127.0.0.1:8080",
 	BasePath:         "/api/user",
 	Schemes:          []string{"http", "https"},
