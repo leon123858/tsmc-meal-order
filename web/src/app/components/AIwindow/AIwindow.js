4d5d32610ca2f1acbe73eddd046832ebbf91e1b7
DaiDai924 <chingtingtai924@gmail.com>
2023-12-16 00:04:40 +0800

    update first login

--- web/src/app/components/AIwindow/AIwindow.js
+++ web/src/app/components/AIwindow/AIwindow.js
@@ -1,68 +1,57 @@
 import React from 'react';
-import { ConfigProvider, Button, Modal, Switch } from 'antd';
+import { ConfigProvider, Button, Modal, Input } from 'antd';
 import styles from './AIwindow.module.css';
 
 const AIwindow = ({curAIWindowState, setAIWindowState}) => {
-  return (
-    <>
-        <ConfigProvider
-            theme={{
-                components: {
-                    Modal: {
-                        contentBg: '#e0f2ff',
-                        width: 200
+    const [text, setText] = React.useState("");
+
+    const handleTextChange = (e, setText) => {
+        setText(e.target.value);
+    };
+
+    return (
+        <>
+            <ConfigProvider
+                theme={{
+                    components: {
+                        Modal: {
+                            contentBg: '#e0f2ff',
+                            width: 200
+                        },
                     },
-                },
-            }}
-        >
-            <Modal
-                width="100vw"
-                className={styles.modal_content}
-                open={curAIWindowState}
-                onOk={() => setAIWindowState(false)}
-                onCancel={() => setAIWindowState(false)}
-                footer={[
-                    <div key={"recommend"} className={styles.recommend_div}>                
-                        <Button 
-                            // key="recommend" 
-                            className={styles.recommend_button}
-                            onClick={() => setAIWindowState(false)}
-                        >
-                            推薦
-                        </Button>
-                    </div>
-                ]}
+                }}
             >
-                <div>
-                    <b
-                        face="monospace" 
-                        size="6"
-                    >
-                        篩選條件<br />
-                    </b>
-                    <Switch defaultChecked/>&nbsp;&nbsp;<Switch defaultChecked></Switch>&nbsp;&nbsp;<Switch defaultChecked></Switch>
-                </div>
-                <div>
-                    <b
-                        face="monospace" 
-                        size="6"
-                    >
-                        推薦天數<br />
-                    </b>
-                    <Switch defaultChecked></Switch>&nbsp;&nbsp;<Switch defaultChecked></Switch>
-                </div>
-                <div>
-                    <b
-                        face="monospace" 
-                        size="4"
-                    >
-                        價格上限<br />
-                    </b>
-                    <Switch defaultChecked></Switch>&nbsp;&nbsp;<Switch defaultChecked></Switch>&nbsp;&nbsp;<Switch defaultChecked></Switch>&nbsp;&nbsp;<Switch defaultChecked></Switch>
-                </div>
-            </Modal>
-        </ConfigProvider>
-    </>
-  );
+                <Modal
+                    width="100vw"
+                    className={styles.modal_content}
+                    open={curAIWindowState}
+                    onOk={() => setAIWindowState(false)}
+                    onCancel={() => setAIWindowState(false)}
+                    footer={[
+                        <div key={"recommend"} className={styles.recommend_div}>                
+                            <Button 
+                                // key="recommend" 
+                                className={styles.recommend_button}
+                                onClick={() => setAIWindowState(false)}
+                            >
+                                推薦
+                            </Button>
+                        </div>
+                    ]}
+                >
+                    <div>
+                        <b
+                            face="monospace" 
+                            size="6"
+                        >
+                            請輸入您的需求，我們將為您推薦最適合的餐點！
+                        <br />
+                        </b>
+                        <Input value={text} onChange={(e) => handleTextChange(e, setText)} />
+                    </div>
+                </Modal>
+            </ConfigProvider>
+        </>
+    );
 };
 export default AIwindow;
