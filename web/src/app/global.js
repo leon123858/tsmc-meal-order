6caf94ac46dae346b525300bc16222232eee0c6a
changcc0716057 <jackson31102@gmail.com>
2023-12-17 21:49:25 +0800

    Image Finished

--- web/src/app/global.js
+++ web/src/app/global.js
@@ -1,3 +1,4 @@
 export const OrderAPI = "https://order-kt6w747drq-de.a.run.app/api/order";
 export const MenuAPI = "https://menu-kt6w747drq-de.a.run.app/api/menu";
 export const UserAPI = "https://user-kt6w747drq-de.a.run.app/api/user";
+export const ImageAPI = "https://storage-kt6w747drq-de.a.run.app/api/storage/upload/image"
