762ec19bbf5886027f652cc404458a5e87cd98ea
adamhung <touycvbn95147@gmail.com>
2023-12-18 22:23:05 +0800

    change openai api call to async

--- ai/embeddingGenerator.py
+++ ai/embeddingGenerator.py
@@ -1,10 +1,12 @@
+import asyncio
 import os
+import random
 from typing import List
 
 import numpy as np
 import pandas as pd
 from dotenv import load_dotenv
-from openai import OpenAI
+from openai import AsyncOpenAI
 from pydantic import BaseModel
 
 from model import MenuEmbedding, Menu
@@ -33,7 +35,7 @@ def convert_menu_to_menu_item(menu: Menu) -> List[MenuItem]:
 
 
 class EmbeddingGenerator:
-    client: OpenAI = None
+    client: AsyncOpenAI = None
     isDev: bool = False
 
     def __init__(self):
@@ -44,27 +46,28 @@ class EmbeddingGenerator:
             self.isDev = True
             return
 
-        self.client = OpenAI(api_key=openai_key)
+        self.client = AsyncOpenAI(api_key=openai_key)
 
-    def get_menu_embedding(self, menu: Menu) -> List[MenuEmbedding]:
+    async def get_menu_embedding(self, menu: Menu) -> List[MenuEmbedding]:
         # print(f"Generating embedding for menu")
         menu_items = convert_menu_to_menu_item(menu)
         data_frame = pd.DataFrame([item.dict() for item in menu_items])
         data_frame['combined'] = data_frame['Name'] + ' ' + data_frame['Description'] + ' ' + ' '.join(
             data_frame['Tags'].apply(lambda x: ' '.join(x)))
-        data_frame['ada_embedding'] = data_frame['combined'].apply(lambda x: self.get_embedding(x))
+        data_frame['ada_embedding'] = await asyncio.gather(*(self.get_embedding(x) for x in data_frame['combined']))
 
         return [MenuEmbedding(menuId=row[0], index=row[1], embedding=row[2]) for row in
                 data_frame[['MenuId', 'Index', 'ada_embedding']].values]
 
-    def get_embedding(self, text: str, engine="text-embedding-ada-002") -> List[float]:
+    async def get_embedding(self, text: str, engine="text-embedding-ada-002") -> List[float]:
         if self.isDev:
+            await asyncio.sleep(random.uniform(0.5, 2.0))
             return [float(i) + 0.5 for i in range(1536)]
 
         # print(f"Generating embedding for text: {text}")
         text = text.replace("\n", " ")
 
-        response = self.client.embeddings.create(input=[text], model=engine)
+        response = await self.client.embeddings.create(input=[text], model=engine)
 
         # print(f"Embedding: {response.data[0].embedding}")
 
